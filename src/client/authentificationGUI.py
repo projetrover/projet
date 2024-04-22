95126f85398407e97e71fa1b8bc08ba587ef8a1e
projetrover <163149305+projetrover@users.noreply.github.com>
2024-04-22 22:07:45 +0200

    Add files via upload

--- src/client/authentificationGUI.py
+++ src/client/authentificationGUI.py
@@ -2,8 +2,7 @@ import authentification as aut
 import tkinter as tk
 
 
-
-IMAGE_MARS = "Mars-header-blog.png"
+IMAGE_MARS = "Mars-blog-header.png"
 
 
 class AuthentificationGUI(aut.Authentification):
@@ -14,24 +13,29 @@ class AuthentificationGUI(aut.Authentification):
 		self.window = window
 		self.Canvas = tk.Canvas(self.window,width=1920,height=1080,bg="white")
 		self.bg = tk.PhotoImage(file = Img)
-		self.Canvas.create_image(500,500,image = self.bg)
-		#self.Frame = tk.Frame(self.Canvas,width = 700, height = 500)
-		self.label1 = tk.Label(self.Canvas,text = "Nom d'utilisateur: ",width = 20, height = 5)
-		self.entry1 = tk.Entry(self.window)
-		self.Canvas.create_window(960,500,window=self.entry1)
-		self.label2 = tk.Label(self.Canvas,text = "Mot de passe: ",width = 20, height = 5)
+		self.Canvas.create_image(960,540,image = self.bg)
+		self.Canvas.create_text(960, 100, text="EXPLOVER", font="impact 80 italic", fill="red")
+		self.Canvas.create_text(960, 460, text="Nom d'utilisateur: ", font="calibri 10", fill="black")
+		
 		self.entry2 = tk.Entry(self.window)
-		self.Canvas.create_window(960,580,window=self.entry2)
-		#self.Frame.pack(side = "top")
+		self.Canvas.create_window(960,500,window=self.entry2)
+		self.Canvas.create_text(960, 540, text="Mot de passe: ", font="calibri 10", fill="black")
+
+		self.entry3 = tk.Entry(self.window)
+		self.Canvas.create_window(960,580,window=self.entry3)
+		self.button = tk.Button(self.window,text = "Connexion",command=lambda:self.setcommand(),relief="groove")
+		self.Canvas.create_window(960,620,window = self.button)
+		self.Canvas.pack()	
+			
+	def setcommand(self):
+		print("check")
 		
-		#self.label1.pack()
-		#self.label2.pack()
-		self.Canvas.pack()		
-		
-if __name__ == "__main__":
-	w=tk.Tk()
-	w.wm_attributes('-alpha', 0)	
-	w.resizable(width='false',height='false')
-	w.geometry("1920x1080")
-	A = AuthentificationGUI(w,IMAGE_MARS)
-	tk.mainloop()	
+	
+			
+	
+#w=tk.Tk()
+#w.wm_attributes('-alpha', 0)	
+#w.resizable(width='false',height='false')
+#w.geometry("1920x1080")
+#A = Authentification(w,IMAGE_MARS)
+#tk.mainloop()	
