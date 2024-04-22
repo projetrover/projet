55292f6f916d6ea676095e5e9d406b71cab57956
projetrover <163149305+projetrover@users.noreply.github.com>
2024-04-22 22:53:11 +0200

    Update authentificationGUI.py

--- src/client/authentificationGUI.py
+++ src/client/authentificationGUI.py
@@ -14,28 +14,34 @@ class AuthentificationGUI(aut.Authentification):
 		self.Canvas = tk.Canvas(self.window,width=1920,height=1080,bg="white")
 		self.bg = tk.PhotoImage(file = Img)
 		self.Canvas.create_image(960,540,image = self.bg)
-		self.Canvas.create_text(960, 100, text="EXPLOVER", font="impact 80 italic", fill="red")
+		self.Canvas.create_text(960, 100, text="EXPLOVER", font="impact 80 italic", fill="black")
 		self.Canvas.create_text(960, 460, text="Nom d'utilisateur: ", font="calibri 10", fill="black")
 		
-		self.entry2 = tk.Entry(self.window)
-		self.Canvas.create_window(960,500,window=self.entry2)
+		self.entry1 = tk.Entry(self.window)
+		self.Canvas.create_window(960,500,window=self.entry1)
 		self.Canvas.create_text(960, 540, text="Mot de passe: ", font="calibri 10", fill="black")
 
-		self.entry3 = tk.Entry(self.window)
-		self.Canvas.create_window(960,580,window=self.entry3)
+		self.entry2 = tk.Entry(self.window)
+		self.Canvas.create_window(960,580,window=self.entry2)
 		self.button = tk.Button(self.window,text = "Connexion",command=lambda:self.setcommand(),relief="groove")
 		self.Canvas.create_window(960,620,window = self.button)
 		self.Canvas.pack()	
 			
-	def setcommand(self):
-		print("check")
+	
 		
+	def error_wrong_password(self):
+		self.Canvas.create_text(960, 660, text = "Nom d'utilisateur ou mot de passe incorrect", font = "calibri 10 bold", fill = "red")
+		self.Canvas.pack()
+		
+	def setcommand(self):
+		print("action provisoire")
+		
+		
+if __name__ == "main":
 	
-			
-	
-w=tk.Tk()
-w.wm_attributes('-alpha', 0)	
-w.resizable(width='false',height='false')
-w.geometry("1920x1080")
-A = Authentification(w,IMAGE_MARS)
-tk.mainloop()	
+	w=tk.Tk()
+	w.wm_attributes('-alpha', 0)	
+	w.resizable(width='false',height='false')
+	w.geometry("1920x1080")
+	A = AuthentificationGUI(w,IMAGE_MARS)
+	tk.mainloop()	
