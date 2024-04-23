d9486b5642fbf4ad42ff5487e2bda8152dfd6e8e
projetrover <163149305+projetrover@users.noreply.github.com>
2024-04-23 15:56:01 +0200

    Update main_GUI.py

--- src/client/main_GUI.py
+++ src/client/main_GUI.py
@@ -7,50 +7,52 @@ posx = 960
 posy = 540
 #position de l'image (globale)
 
-def move_left(canvas,bg):
-	global posx
-	global posy
-	posx += 100
-	canvas.create_image(posx,posy,image = bg)
+class mainGUI():
+
+	def __init__(self,window,Img):
 	
-def move_right(canvas,bg):
-	global posx
-	global posy
-	posx -= 100
-	canvas.create_image(posx,posy,image = bg)
-
-def move_up(canvas,bg):
-	global posx
-	global posy
-	posy += 100
-	canvas.create_image(posx,posy,image = bg)
-
-def move_down(canvas,bg):
-	global posx
-	global posy
-	posy -= 100
-	canvas.create_image(posx,posy,image = bg)
-
-w=tk.Tk()
-
-w.wm_attributes('-alpha', 0)	
-w.resizable(width='false',height='false')
-w.geometry("1920x1080")
-Canv = tk.Canvas(w,width=1920,height=1080,bg="white")
-bg = tk.PhotoImage(file = MAP)
-Canv.create_image(960,540,image = bg)
-
-
+		self.window = window
+		self.Canvas = tk.Canvas(self.window,width=1920,height=1080,bg="white")
+		self.bg = tk.PhotoImage(file = Img)
+		self.Canvas.create_image(960,540,image = self.bg)
+		self.Canvas.pack()	
+		
 	
-b = tk.Button(w,text = "left",command=lambda:move_left(Canv,bg),relief="groove")
-Canv.create_window(920,540,window = b)
-b = tk.Button(w,text = "right",command=lambda:move_right(Canv,bg),relief="groove")
-Canv.create_window(1000,540,window = b)
-b = tk.Button(w,text = "up",command=lambda:move_up(Canv,bg),relief="groove")
-Canv.create_window(960,500,window = b)
-b = tk.Button(w,text = "down",command=lambda:move_down(Canv,bg),relief="groove")
-Canv.create_window(960,580,window = b)
+	def move_left(canvas,bg):
+		print("move_left")
+		global posx
+		global posy
+		posx += 12
+		canvas.create_image(posx,posy,image = bg)
+	
+	
+	def move_right(self):
+		print("move_right")
+		global posx
+		global posy
+		posx -= 12
+		canvas.create_image(posx,posy,image = bg)
 
-Canv.pack()
+	def move_up(self):
+		print("move_up")
+		global posx
+		global posy
+		posy += 12
+		canvas.create_image(posx,posy,image = bg)
 
-tk.mainloop()	
+	def move_down(self):
+		print("move_down")
+		global posx
+		global posy
+		posy -= 12
+		canvas.create_image(posx,posy,image = bg)
+
+if __name__ == "__main.py__":
+
+
+	w=tk.Tk()
+	w.wm_attributes('-alpha', 0)	
+	w.resizable(width='false',height='false')
+	w.geometry("1920x1080")
+	A = mainGUI(w,"mercat.png")
+	tk.mainloop()	
